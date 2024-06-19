14a0320cea1f6faad07b5b7b92f56aa3baa17b5e
unknown <fredlukadi53@gmail.com>
2024-06-19 14:52:58 +0100

    creation route

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test',function(){
+    $tableau = array (
+        "fruits"  => array("nom" => "orange", "b" => "banana", "c" => "apple"),
+        "numbers" => array(1, 2, 3, 4, 5, 6),
+        "holes"   => array("first", 5 => "second", "third")
+    );
+    return response()->json($tableau,200);
+});
+
