4e9697e4f2d93b26707b848722afc07601f33a47
unknown <fredlukadi53@gmail.com>
2024-06-28 12:48:36 +0100

    creation auth dans utilisateur

--- app/Http/Controllers/Api/UtilisateurController.php
+++ app/Http/Controllers/Api/UtilisateurController.php
@@ -4,37 +4,57 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Agent;
-use App\Http\Requests\Api\AgentCreateRequest;
+use App\Models\Utilisateur;
+use App\Http\Requests\Api\UtilisateurCreateRequest;
 use Exception;
-use App\Http\Requests\Api\AgentEditorRequest;
-use App\Models\Adresse;
-use App\Models\Agence;
+use App\Http\Requests\Api\UtilisateurEditorRequest;
+use App\Models\Agent;
+use App\Models\Externe;
 
-class AgentController extends Controller
+class UtilisateurController extends Controller
 {
     public function index (){
-        return "Ici sera Dressé la liste des Agents";
+        return "Ici sera Dressé la liste des Utilisateurs";
     }
-    public function create(AgentCreateRequest $req){
+    public function create(UtilisateurCreateRequest $req){
         
         try{
-        $agent_adresse=Adresse::find($req->id_adresse);
-        $agent_agence=Agence::find($req->id_agence);
-        $nouvelle_agent= new Agent();
-        $nouvelle_agent->name_agent=$req->name_agent;
-        $nouvelle_agent->prenom_agent=$req->prenom_agent;
-        $nouvelle_agent->e_mail_agent=$req->e_mail_agent;
-        $nouvelle_agent->phone_agent=$req->phone_agent;
-        $nouvelle_agent->date_de_naissance=$req->date_de_naissance;
-        $nouvelle_agent->fonction=$req->fonction;
-        $nouvelle_agent->id_adresse=$agent_adresse->id;
-        $nouvelle_agent->id_agence=$agent_agence->id;
-        $nouvelle_agent->save();
+        $id_utilisateur_Agent=agent::find($req->id_agent);
+        $id_utilisateur_Externe=externe::find($req->id_externe);
+        if($req->id_agent==null && $req->id_externe==null){
+            return response()->json([
+                "Success"=>false,
+                "Error"=>true,
+                "Message"=>"ça n'as pas aboutis",
+                "Erros list"=>"L'adresse n'as pas été trouvé"
+        ],422);
+        }
+        if($id_utilisateur_Agent && $id_utilisateur_Externe ){
+            return response()->json([
+                "Success"=>false,
+                "Error"=>true,
+                "Message"=>"ça n'as pas aboutis",
+                "Erros list"=>"tu ne peux pas etre à la fois agent et externe"
+        ],500);
+        }
+        if(!$id_utilisateur_Agent && !$id_utilisateur_Externe ){
+            return response()->json([
+                "Success"=>false,
+                "Error"=>true,
+                "Message"=>"ça n'as pas aboutis",
+                "Erros list"=>"Agent et Externe n'ont pzs etait trouvé dans la base donnnée"
+        ],500);
+        }
+        $nouvelle_Utilisateur= new Utilisateur();
+        $nouvelle_Utilisateur->name_Utilisateur=$req->name_Utilisateur;
+        $nouvelle_Utilisateur->prenom_Utilisateur=$req->prenom_Utilisateur;
+        $nouvelle_Utilisateur->id_adresse=$agent_adresse->id;
+        $nouvelle_Utilisateur->id_agence=$agent_agence->id;
+        $nouvelle_Utilisateur->save();
         return response()->json([
             "Status_code"=>201,
-            "Status_message"=>"L'agent a étais ajouté",
-            "Data"=>$nouvelle_agent
+            "Status_message"=>"L'Utilisateur a étais ajouté",
+            "Data"=>$nouvelle_utilisateur
         ],201);
         }catch(Exception $error){
             return response()->json([
@@ -45,26 +65,24 @@ class AgentController extends Controller
         ],500);
         }
     }
-    public function editor(AgentEditorRequest $req, Agent $Agent){
+    public function editor(UtilisateurEditorRequest $req, Utilisateur $Utilisateur){
                
         try {
-            $agent_adresse=Adresse::find($req->id_adresse);            
-            if(!$agent_adresse){
+            $Utilisateur_Adresse=Adresse::find($req->id_adresse);            
+            if(!$utilisateur_Adresse){
                 return response()->json([
                     "Success"=>false,
                     "Error"=>true,
-                    "Message"=>"L'agent n'as pas pu etre modifier",
-                    "Erros list"=>"L'agent n'a pas étais changé"
+                    "Message"=>"L'Utilisateur n'as pas pu etre modifier",
+                    "Erros list"=>"L'Utilisateur n'a pas étais changé"
             ],500);
             }
 
-            $Agent->name_agent=$req->name_agent;
-            $Agent->prenom_agent=$req->prenom_agent;
-            $Agent->e_mail_agent=$req->e_mail_agent;
-            $Agent->phone_agent=$req->phone_agent;
-            $Agent->date_de_naissance=$req->date_de_naissance;
-            $Agent->fonction=$req->fonction;
-            $Agent->save();
+            $Utilisateur->name_Utilisateur=$req->name_Utilisateur;
+            $Utilisateur->password=$req->prenom_Utilisateur;
+            $Utilisateur->e_mail_Utilisateur=$req->e_mail_Utilisateur;
+            $Utilisateur->fonction=$req->fonction;
+            $Utilisateur->save();
 
             }catch(Exception $error){
                 return response()->json([
@@ -76,14 +94,14 @@ class AgentController extends Controller
             }
      
         }
-        public function delete(Agent $Agent){
+        public function delete(Utilisateur $Utilisateur){
             
             try{
-                $Agent->delete();
+                $Utilisateur->delete();
                 return response()->json([
                     "Status_code"=>200,
                     "Status_message"=>"La suppremion à été effectué",
-                    "Data"=>$Agent
+                    "Data"=>$Utilisateur
                 ],200);
 
             }catch(Exception $error){
